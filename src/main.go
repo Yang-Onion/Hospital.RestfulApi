e358b2df9bf986da46f3515fb872cd599d3bf316
Yang-Onion <MyOnion@126.com>
2018-08-31 18:05:08 +0800

    test mux param

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	c "./controller"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -12,7 +13,7 @@ func main() {
 	//体检报告 每次20条
 	router.HandleFunc("/api/physical/list", c.GetReportList).Methods("GET")
 	//根据体检任务Id获取体检报告
-	router.HandleFunc("/api/physical/list/task/{id}", c.GetReportByTaskId).Methods("GET")
+	router.HandleFunc("/api/physical/list/task/{id}", Handler(c.GetReportByTaskId).Methods("GET")
 	//根据病人Id获取体检报告
 	router.HandleFunc("/api/physical/list/patients/{bingRenId}", c.GetReportByPatientId).Methods("GET")
 	//体检报告 状态回写
@@ -22,6 +23,13 @@ func main() {
 	//体检单位
 	router.HandleFunc("/api/physical/team/list", c.GetTeamList).Methods("GET")
 
+	router.HandleFunc("/test/{id}", Test).Methods("GET")
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 
+}
+func Test(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskid := vars["id"]
+	json.NewEncoder(w).Encode(taskid)
 }
